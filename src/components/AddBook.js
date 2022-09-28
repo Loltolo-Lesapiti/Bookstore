c98c8ff7f2f116ac87758e050ee7cee44251c6a6
Petro Lesapiti <petrolesapiti@gmail.com>
2022-09-28 16:44:01 +0300

    Display books and add button functionality

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,13 +1,68 @@
-import React from 'react';
+import uuid from "react-uuid";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addBook, deleteBook } from "../redux/books/books";
 
-const AddBook = () => (
-  <div className="addBook">
-    <h1>Add Books</h1>
-    <form>
-      <input type="text" name="name" placeholder="Tittle" />
-      <input type="text" placeholder="Author" />
-      <button type="submit">Add Book</button>
-    </form>
-  </div>
-);
+const AddBook = () => {
+  const dispatch = useDispatch();
+  const bookList = useSelector((state) => state.books.value);
+
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  return (
+    <div className="addBook">
+      <h1>Add Books</h1>
+      <div className="displayBook">
+        {bookList.map((book) => (
+          <div className="booksList" key={book}>
+            <div className="book">
+              <h3 className="name">{book.title}</h3>
+              <h3 className="author">{book.author}</h3>
+            </div>
+            <button
+              type="button"
+              onClick={() => {
+                dispatch(deleteBook({ id: book.id }));
+              }}
+            >
+              Remove
+            </button>
+          </div>
+        ))}
+      </div>
+      <form>
+        <input
+          type="text"
+          name="title"
+          placeholder="Tittle"
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          onChange={(event) => {
+            setAuthor(event.target.value);
+          }}
+        />
+        <button
+          type="button"
+          onClick={() => {
+            dispatch(
+              addBook({
+                id: uuid(),
+                title,
+                author,
+              })
+            );
+          }}
+        >
+          {" "}
+          Add Book
+        </button>
+      </form>
+    </div>
+  );
+};
 export default AddBook;
