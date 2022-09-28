0111e0bb3cad2c68de26ff86563b07b8c4de1276
Petro Lesapiti <petrolesapiti@gmail.com>
2022-09-28 16:42:41 +0300

    Create a file containing books data

---
+++ src/redux/books/data.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+export const books = [
+  {
+    id: 1,
+    title: "Damu Nyeusi",
+    author: "Ken Walibora",
+  },
+  {
+    id: 2,
+    title: "The River Between",
+    author: "Ngugi Wa Thiongo",
+  },
+];
