138ddd6e4e181c12bd65139628c709e96bb6dc38
Petro Lesapiti <petrolesapiti@gmail.com>
2022-09-28 16:45:51 +0300

    Update display

---
+++ src/components/BooksContainer.js
@@ -0,0 +1,13 @@
+// This component displays list of books and removes the book.
+import AddBook from './AddBook';
+
+function App() {
+  return (
+    <div className="App">
+      {' '}
+      <AddBook />
+    </div>
+  );
+}
+
+export default App;
