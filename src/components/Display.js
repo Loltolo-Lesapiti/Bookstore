7a1074cbe7feaa92a0204312f7d7b990afe8f888
Petro Lesapiti <petrolesapiti@gmail.com>
2022-10-07 14:19:02 +0300

    Update display

--- src/components/Display.js
+++ src/components/Display.js
@@ -14,26 +14,13 @@ const Books = () => {
 
   return (
     <div className="main_container">
-      <div className="Lesson-Panel">
+      <div>
         <div className="BooksDisplay">
           {booksList.length > 0 ? (
             <BookContainer bookList={booksList} />
           ) : (
-            <h3 className="bookContainer">The store is empty</h3>
+            <h3 className="Lesson-Panel">The store is empty</h3>
           )}
-        </div>
-        <div className="progress">
-          <div className="Oval-2"></div>
-          <div className="flex-col">
-            <span className="-Percent-Complete">64%</span>
-            <span className="Completed Text-Style-2">Completed</span>
-          </div>
-        </div>
-        <div className="Line-2"></div>
-        <div className="flex-col">
-          <span className="Current-Chapter">Current Chapter</span>
-          <span className="Current-Lesson Text-Style-4">Chapter 17</span>
-          <span className="Update-progress">Update progress</span>
         </div>
       </div>
       <AddBook />
